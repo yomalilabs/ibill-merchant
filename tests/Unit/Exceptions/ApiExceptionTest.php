e51486d7870dd9f80c106e234f4b7487754b7607
Ben-Piet O'Callaghan <bpocallaghan@gmail.com>
2021-07-19 13:11:10 +0100

    Api Exception Test

---
+++ tests/Unit/Exceptions/ApiExceptionTest.php
@@ -0,0 +1,19 @@
+<?php
+
+use IBill\Config;
+use IBill\Exceptions\ApiException;
+use IBill\IBillClient;
+use PHPUnit\Framework\TestCase;
+
+class ApiExceptionTest extends TestCase
+{
+    /** @test */
+    public function can_throw_exception_with_error()
+    {
+        try {
+            throw new ApiException('something went wrong');
+        } catch (ApiException $exception) {
+            $this->assertEquals('something went wrong', $exception->error);
+        }
+    }
+}
