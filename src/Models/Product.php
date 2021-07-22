96a7e4eca075fb42c4fff94a264bcf70ddde9cb8
Ben-Piet O'Callaghan <bpocallaghan@gmail.com>
2021-07-22 20:58:01 +0100

    Fix product validation on codename

--- src/Models/Product.php
+++ src/Models/Product.php
@@ -2,6 +2,8 @@
 
 namespace IBill\Models;
 
+use IBill\Exceptions\ApiException;
+
 class Product extends Model
 {
     public $amount;
@@ -10,7 +12,12 @@ class Product extends Model
 
     public function __construct(array $options = null)
     {
+        // codename can not contain hyphen
         if (isset($options['codename'])) {
+            if (strpos($options['codename'], '-') !== false) {
+                throw new ApiException("Product codename can not contain a hyphen.");
+            }
+
             $this->codename = $options['codename'];
         }
         if (isset($options['amount'])) {
