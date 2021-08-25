3df7f0baa4a0ce2885e37fac53955bb58d09f6fe
Ben-Piet O'Callaghan <bpocallaghan@gmail.com>
2021-08-25 15:44:56 +0100

    Capture Authorized Payment Request Tests

---
+++ tests/Feature/Requests/CaptureAuthorizedPaymentRequestTest.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Tests\Feature\Requests;
+
+use IBill\Exceptions\ApiException;
+use IBill\IBill;
+use IBill\Models\CaptureAuthorizedPayment;
+use Tests\TestCase;
+
+class CaptureAuthorizedPaymentRequestTest extends TestCase
+{
+    private function validParams($overrides = [])
+    {
+        return array_merge([
+            'amount' => 1025,
+            'authorized_payment_id' => 'AMB8PUM9HPLZ7NJ',
+        ], $overrides);
+    }
+
+    /** @test */
+    public function create_checkout()
+    {
+        $client = new IBill([
+            'account_id' => 6509,
+            'access_token' => 'access-token',
+        ]);
+
+        try {
+            $checkout = new CaptureAuthorizedPayment($this->validParams());
+            $response = $client->captureAuthorizedPayment($checkout);
+        } catch (ApiException $e) {
+        }
+
+        $this->assertEquals(1, $response->success);
+        $this->assertNull($response->error);
+        $this->assertNotNull($response->id);
+        $this->assertNotNull($response->message);
+        $this->assertEquals('Approved and Completed', $response->message);
+    }
+
+    /** @test */
+    public function validate_card_number()
+    {
+        $client = new IBill([
+            'account_id' => 6509,
+            'access_token' => 'faulty-token'
+        ]);
+
+        try {
+            $checkout = new CaptureAuthorizedPayment(
+                $this->validParams([
+                    'authorized_payment_id' => 'faulty',
+                ])
+            );
+            $response = $client->captureAuthorizedPayment($checkout);
+        } catch (ApiException $e) {
+            $this->assertNotNull($e->message);
+        }
+    }
+}
