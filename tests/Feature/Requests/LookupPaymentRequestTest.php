4ada19ed33c0caaabe8bef65fc7d87557464ff13
Ben-Piet O'Callaghan <bpocallaghan@gmail.com>
2021-08-31 12:24:25 +0100

    Lookup Payment Request and Response

---
+++ tests/Feature/Requests/LookupPaymentRequestTest.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Tests\Feature\Requests;
+
+use IBill\Exceptions\ApiException;
+use IBill\IBill;
+use IBill\Models\AuthorizeAndCapturePayment;
+use IBill\Models\ChargePayment;
+use IBill\Models\VoidPayment;
+use Tests\TestCase;
+
+class LookupPaymentRequestTest extends TestCase
+{
+    /** @test */
+    public function do_lookup_request()
+    {
+        $client = new IBill([
+            'account_id' => 6509,
+            'access_token' => 'access-token',
+        ]);
+
+        try {
+            $response = $client->chargePayment($this->validChargePaymentModel());
+
+            $checkout = new VoidPayment([
+                'payment_id' => $response->id,
+            ]);
+            $response = $client->voidPayment($checkout);
+        } catch (ApiException $e) {
+        }
+
+        $this->assertEquals(1, $response->success);
+        $this->assertNull($response->error);
+        $this->assertNotNull($response->id);
+        $this->assertNotNull($response->message);
+        $this->assertEquals('Approved and Completed', $response->message);
+    }
+
+    /** @test */
+    public function validate_payment_id()
+    {
+        $client = new IBill([
+            'account_id' => 6509,
+            'access_token' => 'faulty-token'
+        ]);
+
+        try {
+            $checkout = new VoidPayment(
+                [
+                    'payment_id' => 'faulty',
+                ]
+            );
+            $response = $client->voidPayment($checkout);
+        } catch (ApiException $e) {
+            $this->assertNotNull($e->message);
+        }
+    }
+}
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,8 +2,27 @@
 
 namespace Tests;
 
+use IBill\Models\ChargePayment;
 use PHPUnit\Framework\TestCase as PHPTestCase;
 
 class TestCase extends PHPTestCase
 {
+    protected function validChargePaymentModel()
+    {
+        $model = new ChargePayment([
+            'firstname' => 'Firstname',
+            'lastname' => 'Lastname',
+            'email' => 'info@example.com',
+            'address' => '1234 Fake Address',
+            'zip' => 12345,
+
+            'amount' => 1525,
+            'card_number' => 6011111111111117,
+            'card_cvv' => 123,
+            'card_expiry_month' => 10,
+            'card_expiry_year' => 2025,
+        ]);
+
+        return $model;
+    }
 }
