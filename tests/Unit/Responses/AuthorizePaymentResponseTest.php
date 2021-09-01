880429a3097f70307415caaed7c283d6b9476372
Ben-Piet O'Callaghan <bpocallaghan@gmail.com>
2021-09-01 18:08:09 +0100

    Add CVV and AVS to the auth and charge responses

--- tests/Unit/Responses/AuthorizePaymentResponseTest.php
+++ tests/Unit/Responses/AuthorizePaymentResponseTest.php
@@ -16,4 +16,40 @@ class AuthorizePaymentResponseTest extends TestCase
 
         $this->assertEquals('abcd1234', $response->id);
     }
+
+    /** @test */
+    public function can_set_cvv_code()
+    {
+        $class = (object) ['cvv_code' => 'cvv1234'];
+        $response = new AuthorizePaymentResponse($class);
+
+        $this->assertEquals('cvv1234', $response->cvv_code);
+    }
+
+    /** @test */
+    public function can_set_cvv_message()
+    {
+        $class = (object) ['cvv_message' => 'CVV Message'];
+        $response = new AuthorizePaymentResponse($class);
+
+        $this->assertEquals('CVV Message', $response->cvv_message);
+    }
+
+    /** @test */
+    public function can_set_avs_code()
+    {
+        $class = (object) ['avs_code' => 'avs1234'];
+        $response = new AuthorizePaymentResponse($class);
+
+        $this->assertEquals('avs1234', $response->avs_code);
+    }
+
+    /** @test */
+    public function can_set_avs_message()
+    {
+        $class = (object) ['avs_message' => 'AVS Message'];
+        $response = new AuthorizePaymentResponse($class);
+
+        $this->assertEquals('AVS Message', $response->avs_message);
+    }
 }
--- tests/Unit/Responses/ChargePaymentResponseTest.php
+++ tests/Unit/Responses/ChargePaymentResponseTest.php
@@ -15,4 +15,40 @@ class ChargePaymentResponseTest extends TestCase
 
         $this->assertEquals('abcd1234', $response->id);
     }
+
+    /** @test */
+    public function can_set_cvv_code()
+    {
+        $class = (object) ['cvv_code' => 'cvv1234'];
+        $response = new ChargePaymentResponse($class);
+
+        $this->assertEquals('cvv1234', $response->cvv_code);
+    }
+
+    /** @test */
+    public function can_set_cvv_message()
+    {
+        $class = (object) ['cvv_message' => 'CVV Message'];
+        $response = new ChargePaymentResponse($class);
+
+        $this->assertEquals('CVV Message', $response->cvv_message);
+    }
+
+    /** @test */
+    public function can_set_avs_code()
+    {
+        $class = (object) ['avs_code' => 'avs1234'];
+        $response = new ChargePaymentResponse($class);
+
+        $this->assertEquals('avs1234', $response->avs_code);
+    }
+
+    /** @test */
+    public function can_set_avs_message()
+    {
+        $class = (object) ['avs_message' => 'AVS Message'];
+        $response = new ChargePaymentResponse($class);
+
+        $this->assertEquals('AVS Message', $response->avs_message);
+    }
 }
